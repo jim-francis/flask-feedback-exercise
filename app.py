0e95b5a1d00230193f64e2a4eb1281b0ded29474
Jimmy Francis <jimmyfrancis@protonmail.com>
2022-03-21 22:33:37 -0400

    Create basic routes

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from re import A
 from flask import Flask, render_template, redirect, session, flash
 from models import connect_db, db, User
-from forms import UserForm
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask-feedback"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "NGE4ev"
 
 connect_db(app)
 
-
 @app.route('/')
 def home_page():
     return redirect('/register')
 
 @app.route('/register', methods=["GET", "POST"])
-def show_register_form():
-    form = UserForm()
+def register_form():
+    form = RegisterForm()
     
     if form.validate_on_submit():
         username = form.username.data
@@ -26,3 +25,33 @@ def show_register_form():
         first_name = form.first_name.data
         last_name = form.last_name.data
         
+        user = User.register(username, password, email, first_name, last_name)
+        
+        db.session.commit()
+        session['username'] = user.username
+        
+        return redirect('/secret')
+    
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=["GET", "POST"])
+def login_form():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        
+        user = User.authenticate(username, password)
+        if user:
+            session['username'] = user.username
+            return redirect('/secret')
+        else:
+            form.username.errors = ["Invalid username/passord."]
+            return redirect('/login', form=form)
+        
+    return render_template('login.html', form=form)
+
+@app.route('/secret')
+def secret_page():
+    return "You made it!"
--- models.py
+++ models.py
@@ -11,23 +11,22 @@ def connect_db(app):
 class User(db.Model):
     __tablename__ = "users"
     
-    username = db.Column(db.Text(20), primary_key=True, unique=True, nullable=False)
+    username = db.Column(db.String(20), primary_key=True, unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
-    email = db.Column(db.text(50), nullable=False)
-    first_name = db.Column(db.Text(30), nullable=False)
-    last_name = db.Column(db.Text(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
     
     @classmethod
     def register(cls, username, password, email, first_name, last_name):
         hashed = bcrypt.generate_password_hash(password)
         hashed_utf8 = hashed.decode("utf8")
-        
         user = cls(
-            username = username
-            password = hashed_utf8
-            email = email
-            first_name = first_name
-            last_name = last_name
+            username=username,
+            password=hashed_utf8,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
         )
         
         db.session.add(user)
